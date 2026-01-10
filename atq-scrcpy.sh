e0f5b23b3fdc62ddbf60ce3aa8ff0b0bb8130f4a
MikeCat2008 <samuelgargri@gmail.com>
2026-01-10 19:16:50 +0100

    Update atq-scrcpy.sh: Improved script feedback and added more comments

--- atq-scrcpy.sh
+++ atq-scrcpy.sh
@@ -1,38 +1,54 @@
 #!/data/data/com.termux/files/usr/bin/bash
 # ANDROID TO QUEST SCRCPY
 
+# --- ICONS ---
+# Colors
+GREEN='\033[0;32m'
+BLUE='\033[0;34m'
+YELLOW='\033[1;33m'
+RED='\033[0;31m'
+NC='\033[0m' # No Color (Reset)
 
-# ENVIROMENT
+# State Icons
+OK="[  ${GREEN}OK${NC}  ]"
+INFO="[ ${BLUE}INFO${NC} ]"
+WAIT="[ ${YELLOW}WAIT${NC} ]"
+EMPTY="        "
+
+# --- ENVIROMENT ---
 export DISPLAY=:0
 
+# Hidden temporaly file to verify if Openbox has initialiced
 tmp_ready_file="$TMPDIR/.ilovefurries-uwu" # nobody's gonna know :3 - MikeCat2008
 rm -f $tmp_ready_file
 
-
-# CLEANUP FUNCION ON EXIT
+# --- CLEANUP FUNCION ---
 cleanup(){
-    echo -e "\n[] Cleaning up ..."
+    echo -e "${INFO} Cleaning up ..."
     rm -f $tmp_ready_file
 
-    echo "[] Stopping backround processes"
+    echo -e "${INFO} Stopping backround processes"
     kill $(jobs -p) 2>/dev/null
+
+    echo -e "${OK} Cleanup complete. Bye! :3"
 }
 
+# Trap signal EXIT to ensure cleanup() allways get executed
 trap cleanup EXIT
 
-
-# TERMUX-X11 WITH OPENBOX
-echo "[] Starting Termux-X11 with Openbox..."
+# --- TERMUX-X11 WITH OPENBOX ---
+echo -e "${INFO} Starting Termux-X11 with Openbox..."
+# Launch Termux-X11 
 termux-x11 :0 -xstartup "openbox --startup 'touch $tmp_ready_file'" &
 
-echo "[] Waiting for Openbox..."
+# Wait loop until Openbox initialices
+echo -e "${INFO} Waiting for Openbox..."
 until [ -f $tmp_ready_file ]; do
     sleep 0.1
 done
-echo "[] Openbox is ready"
+echo -e "${OK} Openbox is ready"
 
-
-# SCRCPY
+# --- SCRCPY ---
 scrcpy_flags=(
     --fullscreen            # Start in fullscreen mode
     --video-codec=h264      # Use H.264 for wide compatibility and low latency
@@ -44,7 +60,7 @@ scrcpy_flags=(
 )
 
 # Launch scrcpy with the defined flags
-echo "[] Starting scrcpy..."
+echo -e "${INFO} Starting scrcpy...\n${EMPTY} End scrcpy with SIGINT (Ctrl+C)"
 scrcpy "${scrcpy_flags[@]}"
 
-echo "[] scrcpy process has ended"
+echo -e "${INFO} scrcpy process has ended"
